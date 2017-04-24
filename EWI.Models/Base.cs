a169e8da6795244683d4ff2d7c13fca404681412
przemek <przemekglinkowski@gmail.com>
2017-04-24 14:47:46 +0200

    powstanie

---
+++ EWI.Models/Base.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+
+
+namespace EWI.Models
+{
+    public abstract class Base : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+    }
+}
---
+++ EWI.Models/Urzadzenie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EWI.Models
+{
+    public class Urzadzenie
+    {
+        public int id { get; set; }
+        public string nrEwidencji 
+       
+    }
+}
